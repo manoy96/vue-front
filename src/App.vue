0afb0a7bc370d9a0047d72ec8bdd85d03232dfd0
manoy96 <mfewcar@gmail.com>
2019-04-09 12:10:09 -0600

    trying to get router to work

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,31 @@
 <template>
-  <v-app>
-     <main-header></main-header>
+  <div id="app">
+    <main-header></main-header>
     <!-- <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Server</span>
         <span class="font-weight-light">Practice</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-    </v-toolbar> -->
+    </v-toolbar>-->
 
-    <v-content>
-      <HelloWorld/>
-    </v-content>
-  </v-app>
+  </div>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import HelloWorld from './components/HelloWorld'
-
+import Header from "./components/Header.vue";
+import HelloWorld from "./components/HelloWorld";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld, 
-    'main-header': Header,
+    HelloWorld,
+    "main-header": Header
   },
-  data () {
+  data() {
     return {
       //
-    }
+    };
   }
-}
+};
 </script>
