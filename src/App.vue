cc98df1bd42b3f0e21cd0361f9aa8134cfac3005
manoy96 <mfewcar@gmail.com>
2019-04-10 15:30:54 -0600

    Playing with cards

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,69 @@
 <template>
   <div id="app">
-    <main-header></main-header>
-    <!-- <v-toolbar app>
+    <v-toolbar app>
       <v-toolbar-title class="headline text-uppercase">
-        <span>Server</span>
-        <span class="font-weight-light">Practice</span>
+        <span>Rockets</span>
       </v-toolbar-title>
       <v-spacer></v-spacer>
-    </v-toolbar>-->
+      <v-btn flat href="https://github.com/manoy96/vue-front">
+        <span class="mr-2">GitHub</span>
+      </v-btn>
+    </v-toolbar>
+
+
+
+
+
+<v-layout row mt-5 pt-5>
+    <v-flex xs12 sm6 offset-sm3>
+      <v-card>
+        <v-img
+          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
+          height="200px"
+        >
+        </v-img>
+
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">Top western road trips</div>
+            <span class="grey--text">1,000 miles of wonder</span>
+          </div>
+        </v-card-title>
+
+        <v-card-actions>
+          <v-btn flat>Share</v-btn>
+          <v-btn flat color="purple">Explore</v-btn>
+          <v-spacer></v-spacer>
+          <v-btn icon @click="show = !show">
+            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+          </v-btn>
+        </v-card-actions>
+
+        <v-slide-y-transition>
+          <v-card-text v-show="show">
+            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
+          </v-card-text>
+        </v-slide-y-transition>
+      </v-card>
+    </v-flex>
+  </v-layout>
+
+
+
+
+
+
+
 
   </div>
 </template>
 
 <script>
-import Header from "./components/Header.vue";
-import HelloWorld from "./components/HelloWorld";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld,
-    "main-header": Header
-  },
-  data() {
-    return {
-      //
-    };
-  }
+  components: {},
+   data: () => ({
+      show: false
+    })
 };
 </script>
