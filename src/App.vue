883013aa1c00a07ddedd8fe326e442c67e71ec2d
manoy96 <mfewcar@gmail.com>
2019-04-22 23:18:26 -0600

    got simple query working

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,50 @@
 <template>
-  <div id="app">
-    <v-toolbar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Rockets</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn flat href="https://github.com/manoy96/vue-front">
-        <span class="mr-2">GitHub</span>
-      </v-btn>
-    </v-toolbar>
-
-
-
-
-<v-layout row mt-5 pt-5>
-    <v-flex xs12 sm6 offset-sm3>
-      <v-card>
-        <v-img
-          src="https://cdn.vuetifyjs.com/images/cards/sunshine.jpg"
-          height="200px"
-        >
-        </v-img>
-
-        <v-card-title primary-title>
-          <div>
-            <div class="headline">Top western road trips</div>
-            <span class="grey--text">1,000 miles of wonder</span>
-          </div>
-        </v-card-title>
-
-        <v-card-actions>
-          <v-btn flat>Share</v-btn>
-          <v-btn flat color="purple">Explore</v-btn>
-          <v-spacer></v-spacer>
-          <v-btn icon @click="show = !show">
-            <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-          </v-btn>
-        </v-card-actions>
-
-        <v-slide-y-transition>
-          <v-card-text v-show="show">
-            I'm a thing. But, like most politicians, he promised more than he could deliver. You won't have time for sleeping, soldier, not with all the bed making you'll be doing. Then we'll go with that data file! Hey, you add a one and two zeros to that or we walk! You're going to do his laundry? I've got to find a way to escape.
-          </v-card-text>
-        </v-slide-y-transition>
-      </v-card>
-    </v-flex>
-  </v-layout>
-
-<rocketCard/>
-
-
-
-
-
-
+  <div>
+    <v-btn color="info" v-on:click="getAllGraphql()">All</v-btn>
+    {{myItem}}
+    error: {{error}}
   </div>
 </template>
 
 <script>
-import rocketCard from './components/rocketCard'
-
+import gql from "graphql-tag";
 export default {
-  name: "App",
-  components: {
-    rocketCard,
+  data() {
+    return {
+      error: "",
+      myItem: {}
+    };
   },
-   data: () => ({
-      show: false
-    })
+  methods: {
+    getAllGraphql: function() {
+      this.$apollo
+        .query({
+          query: gql`
+            query allRocket {
+              rockets {
+                name
+                country
+                id
+                cost
+                payloadLeo
+                payloadGto
+                kgLeo
+                kgGto
+                url
+              }
+            }
+          `
+        })
+        .then(res => {
+          this.myItem = res.data;
+        })
+        .catch(err => {
+          this.error = err;
+        });
+    }
+  }
 };
 </script>
+
+<style>
+</style>
--- src/components/rocketCard.vue
+++ src/components/rocketCard.vue
@@ -1,12 +1,38 @@
 <template>
-    <ul>
-        <li> test: {{ item.name }}</li>
-    </ul>
+    <v-card>
+    <v-card-title primary-title>
+      <div>
+        <h3 class="headline mb-0">{{ rocket.name.charAt(0).toUpperCase()}}</h3>
+        <ul>
+            <li>{{ rocket.country }}</li>
+        </ul>
+      </div>
+    </v-card-title>
+  </v-card>
     
 </template>
 
 <script>
+import gql from "graphql-tag";
+
 export default {
+    props: {
+        rocket: {
+        type: Object,
+        required: true
+        }
+  },
+//   apollo: {
+//       rocket: gql `query {
+//           rocket {
+//               id
+//               name
+//               country
+//           }
+
+//       }`
+//   }
+
     
 }
 </script>
